0955621b3deda32999e9bb05a085a600b3e95072
khan14 <khankahloon@hotmail.com>
2019-11-12 15:06:07 +0100

    --allow-empty

---
+++ khan 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int arg, char **argv)
+{
+    int i, n;
+    n=100;
+    printf("print all digit 0 to 1000, ending with 5\n");
+    for(i=0; i<=n; i++)
+        {
+
+        if(i%10==5)
+{
+
+
+      printf("%d\n", i);
+
+      }
+    }
+
+    return 0;
+}
